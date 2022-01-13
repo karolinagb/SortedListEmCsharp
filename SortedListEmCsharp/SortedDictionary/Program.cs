1ac1e416ef19dec894679b26d40960b3aae08df2
x <karolina_bp@hotmail.com>
2022-01-13 10:06:42 -0300

    Aprendendo sobre sobre SortedDictionary e SortedSet em C#

---
+++ SortedListEmCsharp/SortedDictionary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortedDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SortedDictionary é em formato de arvore e fica mais facil uma inserção e remoção de elementos
+            //Diferente do sortedList que e uma lista e ela tem q se reordenar quando sao excluidos itens
+            IDictionary<string, Aluno> sorted = new SortedDictionary<string, Aluno>();
+
+            sorted.Add("VT", new Aluno("Vanessa", 34672));
+            sorted.Add("AL", new Aluno("Ana", 5617));
+            sorted.Add("RN", new Aluno("Rafael", 17645));
+            sorted.Add("WM", new Aluno("Wanderson", 11287));
+            sorted.Add("BM", new Aluno("BM", 11287));
+
+            Console.WriteLine();
+            foreach (var item in sorted)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ SortedListEmCsharp/SortedSet/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortedSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Conjunto de alunos:
+            ISet<string> alunos
+                = new SortedSet<string>(new ComparadorMinusculo())
+                {
+                    "Vanessa Tonini",
+                    "Ana Losnak",
+                    "Rafael Nercessian",
+                    "Priscila Stuani"
+                };
+
+            //adicionar: Rafael Rollo
+            alunos.Add("Rafael Rollo");
+            //adicionar: Fabio Gushiken
+            alunos.Add("Fabio Gushiken");
+            //adicionar: Fabio Gushiken
+            alunos.Add("Fabio Gushiken");
+            //adicionar: FABIO GUSHIKEN
+            alunos.Add("FABIO GUSHIKEN");
+
+            foreach (var aluno in alunos)
+            {
+                Console.WriteLine(aluno);
+            }
+
+            ISet<string> outroConjunto = new HashSet<string>();
+
+            //Se todos os elementos de aluno pertece ao outro conjunto
+            //este conjunto é subconjunto do outro? IsSubsetOf
+            alunos.IsSubsetOf(outroConjunto);
+
+            //Se o outro conjunto está contido dentro de aluno
+            //este conjunto é superconjunto do outro? IsSupersetOf
+            alunos.IsSupersetOf(outroConjunto);
+
+            //os conjuntos contêm os mesmos elementos? SetEquals
+            alunos.SetEquals(outroConjunto);
+
+            //subtrai os elementos da outra coleção que também estão neste conjunto - ExceptWith
+            alunos.ExceptWith(outroConjunto);
+
+            //intersecção dos conjuntos - IntersectWith
+            alunos.IntersectWith(outroConjunto);
+
+            //somente em um ou outro conjunto - SymmetricExceptWith
+            alunos.SymmetricExceptWith(outroConjunto);
+
+            //união de conjuntos - UnionWith
+            alunos.UnionWith(outroConjunto);
+        }
+    }
+
+    internal class ComparadorMinusculo : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            return string.Compare(x, y, StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
